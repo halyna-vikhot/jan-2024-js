8f88113a742e2838b1be5550ab039c3282b4b36a
halyna-vikhot <vikhotg77@gmail.com>
2024-03-15 00:20:37 +0200

    add task 6 done

--- Lesson 6/Homework6.js
+++ Lesson 6/Homework6.js
@@ -61,56 +61,124 @@
 //
 // ==========================
 // - є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
 // -- відсортувати його за спаданням за monthDuration
 
-let arrSort = coursesAndDurationArray
-    .sort((a, b) =>
-        b.monthDuration - a.monthDuration);
-console.log(arrSort);
-// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// let arrSort = coursesAndDurationArray
+//     .sort((a, b) =>
+//         b.monthDuration - a.monthDuration);
+// console.log(arrSort);
 
-let arrFilter = coursesAndDurationArray.filter(item => item.monthDuration > 5);
-console.log(arrFilter);
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+//
+// let arrFilter = coursesAndDurationArray.filter(item => item.monthDuration > 5);
+// console.log(arrFilter);
+
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 
-let arrMap = coursesAndDurationArray.map((item, index) => {
-    return {
-        id: index + 1,
-        ...item
-    }
-})
-console.log(arrMap);
+// let Map = coursesAndDurationArray.map((user, index) => {
+//     return {
+//         id: index + 1,
+//         user
+//     }
+// })
+// console.log(Map);
 // =========================
 //     описати колоду карт (від 6 до туза без джокерів)
-// - знайти піковий туз
-// - всі шістки
-// - всі червоні карти
-// - всі буби
-// - всі трефи від 9 та більше
-//
+
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
 //         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
 //     color:'', // 'red','black'
 // }
+let cards = [
+    {cardSuit: 'spades', value: '6', color: 'black'},
+    {cardSuit: 'spades', value: '7', color: 'black'},
+    {cardSuit: 'spades', value: '8', color: 'black'},
+    {cardSuit: 'spades', value: '9', color: 'black'},
+    {cardSuit: 'spades', value: '10', color: 'black'},
+    {cardSuit: 'spades', value: 'ace', color: 'black'},
+    {cardSuit: 'spades', value: 'jack', color: 'black'},
+    {cardSuit: 'spades', value: 'queen', color: 'black'},
+    {cardSuit: 'spades', value: 'king', color: 'black'},
+    {cardSuit: 'diamonds', value: '6', color: 'red'},
+    {cardSuit: 'diamonds', value: '7', color: 'red'},
+    {cardSuit: 'diamonds', value: '8', color: 'red'},
+    {cardSuit: 'diamonds', value: '9', color: 'red'},
+    {cardSuit: 'diamonds', value: '10', color: 'red'},
+    {cardSuit: 'diamonds', value: 'ace', color: 'red'},
+    {cardSuit: 'diamonds', value: 'jack', color: 'red'},
+    {cardSuit: 'diamonds', value: 'queen', color: 'red'},
+    {cardSuit: 'diamonds', value: 'king', color: 'red'},
+    {cardSuit: 'hearts', value: '6', color: 'red'},
+    {cardSuit: 'hearts', value: '7', color: 'red'},
+    {cardSuit: 'hearts', value: '8', color: 'red'},
+    {cardSuit: 'hearts', value: '9', color: 'red'},
+    {cardSuit: 'hearts', value: '10', color: 'red'},
+    {cardSuit: 'hearts', value: 'ace', color: 'red'},
+    {cardSuit: 'hearts', value: 'jack', color: 'red'},
+    {cardSuit: 'hearts', value: 'queen', color: 'red'},
+    {cardSuit: 'hearts', value: 'king', color: 'red'},
+    {cardSuit: 'clubs', value: '6', color: 'black'},
+    {cardSuit: 'clubs', value: '7', color: 'black'},
+    {cardSuit: 'clubs', value: '8', color: 'black'},
+    {cardSuit: 'clubs', value: '9', color: 'black'},
+    {cardSuit: 'clubs', value: '10', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'},
+    ]
+// - знайти піковий туз
+
+// let ace = cards.filter(item => item.value === 'ace' && item.cardSuit === 'spades');
+// console.log(ace);
 //
+// // - всі шістки
+// let six = cards.filter((unit)=>unit.value === '6');
+// console.log('6');
+//
+// // - всі червоні карти
+//
+// let red = cards.filter((item)=> item.color === 'red');
+// console.log(red)
+//     //
+// // - всі буби
+// let diamonds= cards.filter(item=> item.cardSuit === 'diamonds' );
+// console.log(diamonds);
+//
+// // - всі трефи від 9 та більше
+// let clubs= cards.filter(item=>item.cardSuit === 'clubs').splice(4);
+// console.log(clubs)
+
 // =========================
 //
 //     Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
-// {
-//     spades:[],
-//         diamonds:[],
+// spades:[],
+//     diamonds:[],
 //     hearts:[],
 //     clubs:[]
 // }
+const arrCardsSuit = (arr, cardS) => {
+    return arr.reduce((previous, current) => {
+        if (current.cardSuit === cardS) {
+            return previous.push(current)
+        }
+    }, []);
+}
+
+console.log(arrCardsSuit(cards, 'spades'))
+console.log(arrCardsSuit('diamonds'))
+console.log(arrCardsSuit(cards, 'hearts'))
+console.log(arrCardsSuit(cards, 'clubs'))
+
 // =========================
 //     взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
 // --написати пошук всіх об'єктів, в який в modules є sass
