61b768d8be52feeb65e8b25102a6f65430b84897
halyna-vikhot <vikhotg77@gmail.com>
2024-03-30 00:05:21 +0200

    task 10 part

--- Lesson 11/main-one.js
+++ Lesson 11/main-one.js
@@ -1,50 +1,45 @@
 // - взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
 
+function allCarts() {
+    let url = new URL('https://dummyjson.com/carts');
 
-// function allCards() {
-//     let url = new URL('https://dummyjson.com/carts');
-//
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(value => {
-//             let {carts} = value;
-//             console.log(value)
-//
-//             const divCarts = document.getElementById('cards');
-//             for (const Cart of carts) {
-//                 let {products} = Cart;
-//                 const cartDiv = document.createElement('div');
-//                 divCarts.appendChild(cartDiv)
-//                 for (const product of products) {
-//                     const title = document.createElement('h2');
-//                     const price = document.createElement('div');
-//                     const quantity = document.createElement('div');
-//                     const total = document.createElement('div');
-//                     const discountedPrice = document.createElement('div');
-//                     const img = document.createElement('img');
-//
-//                     title.innerText = products.title;
-//                     price.innerText = `price: ${product.price}`;
-//                     quantity.innerText = `quantity: ${product.quantity}`;
-//                     total.innerText = `total: ${product.total}`;
-//                     discountedPrice.innerText = `discountedPrice: ${product.discountedPrice}`;
-//                     discountPercentage.innerText = `discountedPercentage: ${product.discountPercentage}`
-//                     img.src = product.thumbnail
-//
-//                     Div.append(title, price, quantity, total, discountPercentage, discountedPrice, img);
-//                 }
-//             }
-//         });
-// }
-// allCards()
+    fetch(url)
+        .then(res => res.json())
+        .then(value => {
+            let {carts} = value;
+            console.log(value)
 
+            const divCarts = document.getElementById('carts');
+            for (const Cart of carts) {
+                let {products} = Cart;
+                const divCart = document.createElement('div');
+                divCarts.appendChild(divCart)
+                for (const product of products) {
+                    const title = document.createElement('h2');
+                    const price = document.createElement('div');
+                    const quantity = document.createElement('div');
+                    const total = document.createElement('div');
+                    const discountedPrice = document.createElement('div');
+                    const discountPercentage = document.createElement('div');
+                    const img = document.createElement('img');
 
+                    title.innerText = product.title;
+                    price.innerText = `price: ${product.price}`;
+                    quantity.innerText = `quantity: ${product.quantity}`;
+                    total.innerText = `total: ${product.total}`;
+                    discountedPrice.innerText = `discountedPrice: ${product.discountedPrice}`;
+                    discountPercentage.innerText = `discountedPercentage: ${product.discountPercentage}`
+                    img.src = product.thumbnail
+
+                    divCart.append(title, price, quantity, total, discountPercentage, discountedPrice, img);
+                }
+            }
+        });
+}
+allCarts()
 
 
 
-
-//     - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
-//
 //     - зробити файл users.html
 // з ендпоінту http://jsonplaceholder.typicode.com/users отримати всіх користувачів
 //     вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
---
+++ Lesson 11/main-two.js
@@ -0,0 +1,42 @@
+// - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список
+// //     під час відображення.
+function allRecipes(){
+    let url = new URL('https://dummyjson.com/docs/recipes')
+    fetch(url)
+        .then(res => res.json())
+        .then(json =>{
+            const {recipes} = json;
+            const divRecipe = document.getElementById('recipes')
+            for (const recipe of recipes) {
+                const {ingredients, instructions} = recipe;
+                const divRec = document.createElement('div');
+                const name = document.createElement('h2');
+                const titleIng = document.createElement('h3');
+                const titleInstr =document.createElement('h3');
+                const ulIng = document.createElement('ul');
+                const ulInst = document.createElement('ul');
+
+                name.innerText = recipe.name;
+                titleIng.innerText = 'ingredients'
+                titleInstr.innerText = 'instructions'
+
+                divRecipe.appendChild(divRec)
+                divRec.append(name, titleIng, ulIng, titleInstr, ulInst
+                );
+
+                for (const ingredient of ingredients) {
+                    const li = document.createElement('li');
+                    li.innerText = ingredient;
+                    ulIng.appendChild(li);
+                }
+                for (const instruction of instructions) {
+                    const li = document.createElement('li');
+                    li.innerText = instruction;
+                    ulInst.appendChild(li);
+                }
+            }
+            
+        })
+
+}
+allRecipes()
